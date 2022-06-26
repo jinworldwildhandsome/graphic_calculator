89dce96615eb1ef2572b12232e772d18c1c7d106
jinworldwildhandsome <maraihaidei03@gmail.com>
2022-06-26 13:19:14 +0300

    improve class for memorizing previous functions info at previous.js, create function for drawing previous function at main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 "use strict";
 import { receiveCoeff, addCoefficient } from './functions.js'; 
 import { fabricExceptions, StandartEquation  } from './exceptions.js'; 
+import {Stack} from './previous.js'; 
 window.onload = () => {
     const select = document.getElementById('select');
     select.onchange = () => (function () {
@@ -10,7 +11,7 @@ window.onload = () => {
     const buildButton = document.getElementById('buildGrapf');
     buildButton.onclick = () => drawAllParts();
     const prevDrawButton = document.getElementById("showPreviousGrapf"); 
-    prevDrawButton.onclick = () => remembeGrapf(); 
+    prevDrawButton.onclick = () => drawPreviousGrapf(); 
 };
 const canvas = document.getElementById('Mycanvas'); 
 const ctx = canvas.getContext('2d'); 
@@ -54,31 +55,43 @@ const addChanges = (elementsHtml) =>{
         let element = elementsHtml.get(divValue); 
         element.style.visibility = "visible"; 
     }
-}; 
-  
-const exceptions = ["trigonometric", "inverse", "degree"]; 
-const currentGraf = {};  
+};  
+const exceptions = ["trigonometric", "inverse", "degree"];   
 async function drawAllParts(){
     const type = document.getElementById("select").value;
     const coefficients = receiveCoeff(type); 
     chooseDrawFunction( coefficients, type);  
-    Object.assign( currentGraf, {type, coefficients});
 }
+const stack = new Stack(); 
 function chooseDrawFunction ( coefficients, type) {
+    const grapf = chooseEquationType(type, coefficients); 
+    grapf.clearCanvas(ctx); 
+    grapf.drawAxes(ctx); 
+    drawEquation(grapf); 
+    stack.memoize( type, coefficients); 
+} 
+const chooseEquationType = (type, coefficients) =>{
     let drawedFunction; 
     if( exceptions.includes(type)){
         drawedFunction = fabricExceptions(type, coefficients, axes)
     }else{
         drawedFunction = new StandartEquation( axes, coefficients, type); 
     }
+    return drawedFunction; 
+}; 
+const drawEquation = classEquation =>{
     const divsValues = receiveHtmlValues(elementsHtml); 
-    drawedFunction.clearCanvas(ctx); 
-    drawedFunction.drawAxes(ctx); 
-    drawedFunction.changeColorSize( divsValues, ctx );
+    ctx.beginPath();//classEquation.drawAxes(ctx);   
+    classEquation.changeColorSize( divsValues, ctx );
     for( let x = -axes.xCenter; x <= axes.xCenter; x += difference){
-        drawedFunction.drawFullGraf(x, ctx, divsValues[0]); 
+        classEquation.drawFullGraf(x, ctx, divsValues[0]); 
     }
-}
 
-const drawpPreviousGrapf = ( {prevType, prevCoefficients}) => () => chooseDrawFunction( prevType, prevCoefficients); 
-const remembeGrapf = drawpPreviousGrapf(currentGraf); 
+}
+function drawPreviousGrapf (){
+    const funcInfo = stack.getFromMemory(); 
+    console.log(funcInfo.funcType); 
+    const grapf = chooseEquationType( funcInfo.funcType, funcInfo[0]); 
+    console.log(grapf)
+    drawEquation(grapf); 
+}
--- scripts/previous.js
+++ scripts/previous.js
@@ -1,20 +1,32 @@
 "use strict"; 
- 
-const cache = new Map; 
-const generateKey = (name, counter) => name.toString() + counter; 
-const keys = []; 
-let counter; 
+export {Stack}; 
 class Stack{
+    constructor(){
+        this.cache = new Map; 
+        this.counter = 0;
+        this.keys = [];  
+    }
+    generateKey (name, counter) {
+        return  name.toString() + counter;
+    }
     memoize( funcType, ...info){
-        const key = generateKey(funcType, counter); 
-        keys.push(key); 
-        cache.set( key, info);
-        counter++; 
+        const key = this.generateKey(funcType, this.counter); 
+        this.keys.push(key); 
+        const values = Object.assign({}, {...info}, {funcType}); 
+        console.log(values); 
+        console.log({funcType, ...info})
+        this.cache.set( key, values);
+        this.counter++; 
     }
     getFromMemory(){ 
         if(this.counter === 0) alert("All functions are there or you haven't painted any yet!"); 
-        const previousFunc = cache.get( keys[--counter]); 
-        cache.delete(keys[counter]); 
+        console.log(this.counter + "fist")
+        console.log(this.cache.get(this.keys[this.cache]))
+        const previousFunc = this.cache.get( this.keys[--this.counter]); 
+        console.log(this.counter + "seconds")
+        console.log(previousFunc)
+        this.cache.delete(this.keys[this.counter]); 
+        this.keys.splice( this.counter, 1);
         return previousFunc; 
     }
 }
